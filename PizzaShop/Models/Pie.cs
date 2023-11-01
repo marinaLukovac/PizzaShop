b880d8e89aa24b4b99e66ca073a88de9ede9a428
Marina Lukovac <lukovacmarina@gmail.com>
2023-11-01 18:30:13 +0100

    Create Pie class

---
+++ PizzaShop/Models/Pie.cs
@@ -0,0 +1,22 @@
+﻿namespace PizzaShop.Models
+{
+    public class Pie
+    {
+        public int PieID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal  Price { get; set; }
+        public bool IsPieOfTheWeek { get; set; }
+        public bool InStock { get; set; }
+
+        public Pie (int pieID, string name, string description, decimal price, bool isPieOfTheWeek, bool inStock)
+        {
+            PieID = pieID;
+            Name = name;
+            Description = description;
+            Price = price;
+            IsPieOfTheWeek = isPieOfTheWeek;
+            InStock = inStock;
+        }
+    }
+}
